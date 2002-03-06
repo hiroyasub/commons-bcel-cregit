50b8b8ca099bff18afe77cb2fb58640ee1bf7ee9
Markus Dahm <mdahm@apache.org>
2002-03-06 17:50:26 +0000

    doc fixes by dmartin@cliftonlabs.com
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/bcel/trunk@152744 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 45373a17ba9a97e8521f9322578f0a69cf171722

--- src/java/org/apache/bcel/classfile/JavaClass.java
+++ src/java/org/apache/bcel/classfile/JavaClass.java
@@ -60,7 +60,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Represents a Java class, i.e., the data structures, constant pool,  * fields, methods and commands contained in a Java .class file.  * See<a href="ftp://java.sun.com/docs/specs/">JVM   * specification</a> for details.  *  * @version $Id$  * @author<A HREF="mailto:markus.dahm@berlin.de">M. Dahm</A>  */
+comment|/**  * Represents a Java class, i.e., the data structures, constant pool,  * fields, methods and commands contained in a Java .class file.  * See<a href="ftp://java.sun.com/docs/specs/">JVM   * specification</a> for details.   * The intent of this class is to represent a parsed or otherwise existing  * class file.  Those interested in programatically generating classes  * should see the<a href="../generic/ClassGen.html">ClassGen</a> class.   * @version $Id$  * @see org.apache.bcel.generic.ClassGen  * @author<A HREF="mailto:markus.dahm@berlin.de">M. Dahm</A>  */
 end_comment
 
 begin_class
