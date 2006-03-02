43005d92cea892d40dd4d8405da89df90e883e41
Torsten Curdt <tcurdt@apache.org>
2006-03-02 04:10:20 +0000

    fixed an import
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/bcel/trunk@382289 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: fe8fdd95ada8078ce29922f739cad07a7454f700

--- src/java/org/apache/bcel/util/BCELifier.java
+++ src/java/org/apache/bcel/util/BCELifier.java
@@ -37,6 +37,16 @@ end_import
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Locale
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
