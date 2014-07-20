aa48ccb76905e360d8612c23cb5e6dcddb64e7e0
Gary D. Gregory <ggregory@apache.org>
2014-07-20 07:06:54 +0000

    Use the more modern, compact, and flexible Javadoc "{@code ...}" instead of the HTML "<tt>...</tt>".
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1612046 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 205f53aa7ba350b5d1a82192330a36d6feaa4666

--- src/examples/ProxyCreator.java
+++ src/examples/ProxyCreator.java
@@ -222,7 +222,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Dynamically creates and uses a proxy for<tt>java.awt.event.ActionListener</tt>  * via the classloader mechanism if called with  *<pre>java org.apache.bcel.util.JavaWrapper ProxyCreator</pre>  *  * The trick is to encode the byte code we need into the class name  * using the Utility.encode() method. This will result however in big  * ugly class name, so for many cases it will be more sufficient to  * put some clever creation code into the class loader.<br> This is  * comparable to the mechanism provided via  *<tt>java.lang.reflect.Proxy</tt>, but much more flexible.  *  * @version $Id$  * @author<A HREF="mailto:m.dahm@gmx.de">M. Dahm</A>  * @see org.apache.bcel.util.JavaWrapper  * @see org.apache.bcel.util.ClassLoader  * @see Utility  */
+comment|/**  * Dynamically creates and uses a proxy for {@code java.awt.event.ActionListener}  * via the classloader mechanism if called with  *<pre>java org.apache.bcel.util.JavaWrapper ProxyCreator</pre>  *  * The trick is to encode the byte code we need into the class name  * using the Utility.encode() method. This will result however in big  * ugly class name, so for many cases it will be more sufficient to  * put some clever creation code into the class loader.<br> This is  * comparable to the mechanism provided via  * {@code java.lang.reflect.Proxy}, but much more flexible.  *  * @version $Id$  * @author<A HREF="mailto:m.dahm@gmx.de">M. Dahm</A>  * @see org.apache.bcel.util.JavaWrapper  * @see org.apache.bcel.util.ClassLoader  * @see Utility  */
 end_comment
 
 begin_class
