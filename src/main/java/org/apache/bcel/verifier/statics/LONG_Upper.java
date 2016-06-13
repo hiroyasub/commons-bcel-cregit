e9504ae325b40f463695653ce3f77495fd86baee
Gary D. Gregory <ggregory@apache.org>
2016-06-13 08:03:35 +0000

    Better var name.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1748127 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 92a3a06876fc47d55d189f6483b1efc37b1c59bd

--- src/main/java/org/apache/bcel/verifier/statics/LONG_Upper.java
+++ src/main/java/org/apache/bcel/verifier/statics/LONG_Upper.java
@@ -60,7 +60,7 @@ specifier|private
 specifier|static
 specifier|final
 name|LONG_Upper
-name|singleInstance
+name|singleton
 init|=
 operator|new
 name|LONG_Upper
@@ -89,7 +89,7 @@ name|theInstance
 parameter_list|()
 block|{
 return|return
-name|singleInstance
+name|singleton
 return|;
 block|}
 block|}
