d357e115d23e34f2a5532d6dfba29dfe42f4a464
Gary D. Gregory <ggregory@apache.org>
2016-06-13 07:57:22 +0000

    Rename private ivars to remove leading underscore (and a better name).
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1748120 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4a4074b7f330d77f296c16abd82cfe026de31f99

--- src/main/java/org/apache/bcel/generic/FieldGen.java
+++ src/main/java/org/apache/bcel/generic/FieldGen.java
@@ -207,7 +207,7 @@ decl_stmt|;
 specifier|private
 specifier|static
 name|BCELComparator
-name|_cmp
+name|bcelComparator
 init|=
 operator|new
 name|BCELComparator
@@ -1557,7 +1557,7 @@ name|getComparator
 parameter_list|()
 block|{
 return|return
-name|_cmp
+name|bcelComparator
 return|;
 block|}
 comment|/**      * @param comparator Comparison strategy object      */
@@ -1571,7 +1571,7 @@ name|BCELComparator
 name|comparator
 parameter_list|)
 block|{
-name|_cmp
+name|bcelComparator
 operator|=
 name|comparator
 expr_stmt|;
@@ -1589,7 +1589,7 @@ name|obj
 parameter_list|)
 block|{
 return|return
-name|_cmp
+name|bcelComparator
 operator|.
 name|equals
 argument_list|(
@@ -1608,7 +1608,7 @@ name|hashCode
 parameter_list|()
 block|{
 return|return
-name|_cmp
+name|bcelComparator
 operator|.
 name|hashCode
 argument_list|(
