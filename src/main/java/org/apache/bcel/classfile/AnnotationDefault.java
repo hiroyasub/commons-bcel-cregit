566196e8ea4fe8b104328d973c9b1fa686995c64
Gary Gregory <ggregory@rocketsoftware.com>
2019-05-16 14:01:25 -0400

    Remove Subversion's "@version $Id$".
    Former-commit-id: f0523b43b000b1c58b415d9ae003e09629e28fd9

--- src/main/java/org/apache/bcel/classfile/AnnotationDefault.java
+++ src/main/java/org/apache/bcel/classfile/AnnotationDefault.java
@@ -58,7 +58,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Represents the default value of a annotation for a method info  *  * @version $Id: AnnotationDefault 1 2005-02-13 03:15:08Z dbrosius $  * @since 6.0  */
+comment|/**  * Represents the default value of a annotation for a method info  *  * @since 6.0  */
 end_comment
 
 begin_class
