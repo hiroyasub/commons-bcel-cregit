a5c8621d6e8253c5959bb64f2f64f3b2da309821
Gary David Gregory (Code signing key) <ggregory@apache.org>
2022-10-22 10:33:11 -0400

    Remove unused import
    Former-commit-id: fdc70dd3a4ac4a0cb680164bc0bc4150ac2e3d05

--- src/test/java/org/apache/bcel/AbstractTestCase.java
+++ src/test/java/org/apache/bcel/AbstractTestCase.java
@@ -81,18 +81,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|stream
-operator|.
-name|Stream
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
