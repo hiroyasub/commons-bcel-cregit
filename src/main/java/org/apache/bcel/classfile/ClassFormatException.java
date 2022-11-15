c344426c5f291c9c329defd2a9ed0ac03ce98e7f
Gary David Gregory (Code signing key) <ggregory@apache.org>
2022-11-15 07:19:59 -0500

    Add org.apache.bcel.classfile.ClassFormatException.ClassFormatException(Throwable)
    Former-commit-id: eb80c2fb9504e01751cd7a40a52cb840a7f1fb69

--- src/main/java/org/apache/bcel/classfile/ClassFormatException.java
+++ src/main/java/org/apache/bcel/classfile/ClassFormatException.java
@@ -16,7 +16,7 @@ package|;
 end_package
 
 begin_comment
-comment|/**  * Thrown when the BCEL attempts to read a class file and determines that a class is malformed or otherwise cannot be  * interpreted as a class file.  */
+comment|/**  * Thrown when the BCEL attempts to read a class file and determines that a class is malformed or otherwise cannot be interpreted as a class file.  */
 end_comment
 
 begin_class
@@ -35,26 +35,28 @@ init|=
 operator|-
 literal|3569097343160139969L
 decl_stmt|;
+comment|/**      * Constructs a new instance with {@code null} as its detail message. The cause is not initialized, and may subsequently be initialized by a call to      * {@link #initCause}.      */
 specifier|public
 name|ClassFormatException
 parameter_list|()
 block|{
 block|}
+comment|/**      * Constructs a new instance with the specified detail message. The cause is not initialized, and may subsequently be initialized by a call to      * {@link #initCause}.      *      * @param message the detail message. The detail message is saved for later retrieval by the {@link #getMessage()} method.      */
 specifier|public
 name|ClassFormatException
 parameter_list|(
 specifier|final
 name|String
-name|s
+name|message
 parameter_list|)
 block|{
 name|super
 argument_list|(
-name|s
+name|message
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * @since 6.0      */
+comment|/**      * Constructs a new instance with the specified detail message and cause.      *<p>      * Note that the detail message associated with {@code cause} is<i>not</i> automatically incorporated in this runtime exception's detail message.      *      * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method).      * @param cause   the cause (which is saved for later retrieval by the {@link #getCause()} method). (A {@code null} value is permitted, and indicates that      *                the cause is nonexistent or unknown.)      * @since 6.0      */
 specifier|public
 name|ClassFormatException
 parameter_list|(
@@ -71,6 +73,21 @@ name|super
 argument_list|(
 name|message
 argument_list|,
+name|cause
+argument_list|)
+expr_stmt|;
+block|}
+comment|/**      * Constructs a new instance with the specified cause and a detail message of {@code (cause==null ? null : cause.toString())} (which typically contains the      * class and detail message of {@code cause}). This constructor is useful for runtime exceptions that are little more than wrappers for other throwables.      *      * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method). (A {@code null} value is permitted, and indicates that the      *              cause is nonexistent or unknown.)      * @since 6.6.2      */
+specifier|public
+name|ClassFormatException
+parameter_list|(
+specifier|final
+name|Throwable
+name|cause
+parameter_list|)
+block|{
+name|super
+argument_list|(
 name|cause
 argument_list|)
 expr_stmt|;
