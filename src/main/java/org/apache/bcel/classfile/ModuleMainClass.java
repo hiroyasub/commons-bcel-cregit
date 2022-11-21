fa3799f208cc2f34137b7ef3876fe150a61e6453
Gary David Gregory (Code signing key) <ggregory@apache.org>
2022-11-21 16:22:26 -0500

    org.apache.bcel.classfile.ModuleMainClass constructors now throw ClassFormatException on invalid input
    Former-commit-id: 57d1a3c6b8555383e17394dcb37f83f03424f208

--- src/main/java/org/apache/bcel/classfile/ModuleMainClass.java
+++ src/main/java/org/apache/bcel/classfile/ModuleMainClass.java
@@ -57,6 +57,20 @@ name|Const
 import|;
 end_import
 
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|bcel
+operator|.
+name|util
+operator|.
+name|Args
+import|;
+end_import
+
 begin_comment
 comment|/**  * This class is derived from<em>Attribute</em> and indicates the main class of a module. There may be at most one  * ModuleMainClass attribute in a ClassFile structure.  *  * @see Attribute  */
 end_comment
@@ -152,7 +166,14 @@ name|this
 operator|.
 name|mainClassIndex
 operator|=
+name|Args
+operator|.
+name|requireU2
+argument_list|(
 name|mainClassIndex
+argument_list|,
+literal|"mainClassIndex"
+argument_list|)
 expr_stmt|;
 block|}
 comment|/**      * Initialize from another object. Note that both objects use the same references (shallow copy). Use copy() for a      * physical copy.      */
