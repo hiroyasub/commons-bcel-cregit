2bb576d63f0c5e271f3e813c191bd7d1760e5ec0
Gary David Gregory (Code signing key) <ggregory@apache.org>
2023-02-25 08:46:07 -0500

    Javadoc
    Former-commit-id: ca8541e94f4b467bc06d635c3ad4496095c8e235

--- src/main/java/org/apache/bcel/classfile/ClassFormatException.java
+++ src/main/java/org/apache/bcel/classfile/ClassFormatException.java
@@ -56,7 +56,7 @@ name|message
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * Constructs a new instance with the specified detail message and cause.      *<p>      * Note that the detail message associated with {@code cause} is<i>not</i> automatically incorporated in this runtime exception's detail message.      *      * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method).      * @param cause   the cause (which is saved for later retrieval by the {@link #getCause()} method). (A {@code null} value is permitted, and indicates that      *                the cause is nonexistent or unknown.)      * @since 6.0      */
+comment|/**      * Constructs a new instance with the specified detail message and cause.      *      * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method).      * @param cause   the cause (which is saved for later retrieval by the {@link #getCause()} method). A {@code null} value is permitted, and indicates that      *                the cause is nonexistent or unknown.      * @since 6.0      */
 specifier|public
 name|ClassFormatException
 parameter_list|(
@@ -77,7 +77,7 @@ name|cause
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * Constructs a new instance with the specified cause and a detail message of {@code (cause==null ? null : cause.toString())} (which typically contains the      * class and detail message of {@code cause}). This constructor is useful for runtime exceptions that are little more than wrappers for other throwables.      *      * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method). (A {@code null} value is permitted, and indicates that the      *              cause is nonexistent or unknown.)      * @since 6.7.0      */
+comment|/**      * Constructs a new instance with the specified cause and a detail message of {@code (cause==null ? null : cause.toString())} (which typically contains the      * class and detail message of {@code cause}). This constructor is useful for runtime exceptions that are little more than wrappers for other throwables.      *      * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method). A {@code null} value is permitted, and indicates that the      *              cause is nonexistent or unknown.      * @since 6.7.0      */
 specifier|public
 name|ClassFormatException
 parameter_list|(
